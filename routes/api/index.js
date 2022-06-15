6d4a58c891c767959835186e81b82fc1101ca1c6
Sev <simplysimplifyy@gmail.com>
2022-06-15 13:54:17 -0600

    added get functionality for the genre retrieval and the individual id retrieval.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-const userRoutes = require("./user-routes");
+const userRoutes = require('./user-routes');
+const BookGenre = require('./BookGenre')
+const BookID = require('./BookID')
 
 router.use("/users", userRoutes);
+router.use("/books", BookGenre);
+router.use("/id", BookID)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "",
+  secret: "secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,

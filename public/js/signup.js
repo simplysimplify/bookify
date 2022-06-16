f9d8dc62810f44fde43051380ce825f352eb51d4
Alexb <acbird.ab@gmail.com>
2022-06-16 14:17:34 -0600

    authorization

--- public/js/signup.js
+++ public/js/signup.js
@@ -13,7 +13,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/discover");
     } else {
       alert("Failed to sign up.");
     }
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,23 +2,23 @@ const router = require("express").Router();
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-  res.render("homepg");
+  res.render("homepg", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/discover", withAuth, async (req, res) => {
-  res.render("discoverpg");
+  res.render("discoverpg", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/quiz", withAuth, async (req, res) => {
-  res.render("quiz");
+  res.render("quiz", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/login", async (req, res) => {
-  res.render("login");
+  res.render("login", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/signUp", async (req, res) => {
-  res.render("signUp");
+  res.render("signUp", { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;

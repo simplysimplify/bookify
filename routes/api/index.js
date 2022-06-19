3679978a4fb0df033455724d46da34f4cb41b67b
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-19 12:44:21 -0600

    trying to figure out POST route for booklogs

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const userRoutes = require('./user-routes');
 const BookGenre = require('./BookGenre')
 const BookID = require('./BookID')
-const BookReview =require('./booklog-routes')
+const BookLog =require('./booklog-routes')
 
 router.use("/users", userRoutes);
 router.use("/books", BookGenre);
 router.use("/id", BookID)
-router.use("/review", BookReview)
+router.use("/booklog", BookLog)
 
 module.exports = router;

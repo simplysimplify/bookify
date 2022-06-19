bd66aeecdd39e5f11f83e5e5a3ff3c392199b354
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-19 12:48:31 -0600

    fixed merge conflict in routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,13 @@ const router = require("express").Router();
 const userRoutes = require('./user-routes');
 const BookGenre = require('./BookGenre')
 const BookID = require('./BookID')
+const BookRecs = require("./bookRecs");
 const BookLog =require('./booklog-routes')
 
 router.use("/users", userRoutes);
 router.use("/books", BookGenre);
 router.use("/id", BookID)
+router.use("/bookrecs", BookRecs);
 router.use("/booklog", BookLog)
 
 module.exports = router;

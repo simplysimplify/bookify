56561bb98dfee1bad31b44cd2bf396b85c51eb4b
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-20 11:50:27 -0600

    index fix

--- models/index.js
+++ models/index.js
@@ -1,18 +1,7 @@
 const User = require("./user");
-const Book = require('./Book');
 const Review = require('./Review')
 //a review belongs to one book
 
-Review.belongsTo(Book, {
-    foreignKey: 'bookId',
-    onDelete: 'CASCADE'
-});
-
-//user has many reviews
-User.hasMany(Review, {
-    foreignKey: 'postId',
-    onDelete: 'CASCADE'
-});
 
 //a review belongs to one user 
 Review.belongsTo(User, {
@@ -26,6 +15,5 @@ User.hasMany(Review, {
 });
 module.exports = {
     User,
-    Book,
     Review
 };
